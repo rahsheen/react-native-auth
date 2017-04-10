917f3f02d21cea809f37883999fa97cf0b4861ff
R. Anthony Porter <rah@sheenonline.biz>
2017-04-10 01:18:01 -0400

    Custom Input element, LoginForm structure.

--- src/app.js
+++ src/app.js
@@ -2,26 +2,27 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import firebase from 'firebase';
 import { Header } from './components/common/';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
 
     componentWillMount() {
         //Firebase init
-        firebase.initializeApp({
-            apiKey: "AIzaSyA714cB9FYzxMnBa1DXEfbnrFeSxHDpOjM",
-            authDomain: "react-native-auth-a222b.firebaseapp.com",
-            databaseURL: "https://react-native-auth-a222b.firebaseio.com",
-            projectId: "react-native-auth-a222b",
-            storageBucket: "react-native-auth-a222b.appspot.com",
-            messagingSenderId: "270065831633"
-        });
+        // firebase.initializeApp({
+        //     apiKey: "AIzaSyA714cB9FYzxMnBa1DXEfbnrFeSxHDpOjM",
+        //     authDomain: "react-native-auth-a222b.firebaseapp.com",
+        //     databaseURL: "https://react-native-auth-a222b.firebaseio.com",
+        //     projectId: "react-native-auth-a222b",
+        //     storageBucket: "react-native-auth-a222b.appspot.com",
+        //     messagingSenderId: "270065831633"
+        // });
     }
 
     render() {
         return (
             <View>
                 <Header headerText="Authentication" />
-                <Text>An App!</Text>
+                <LoginForm />
             </View>
         );
     }
