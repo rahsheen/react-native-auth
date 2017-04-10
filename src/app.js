b075ba2e821059f38a7d5463ea7ebf2d620718b3
R. Anthony Porter <rah@sheenonline.biz>
2017-04-10 00:25:35 -0400

    Imported and initialized Firebase

--- src/app.js
+++ src/app.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
-import { View, Text} from 'react-native';
-import {Header} from './components/common/';
+import { View, Text } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common/';
 
 class App extends Component {
+
+    componentWillMount() {
+        //Firebase init
+        firebase.initializeApp({
+            apiKey: "AIzaSyA714cB9FYzxMnBa1DXEfbnrFeSxHDpOjM",
+            authDomain: "react-native-auth-a222b.firebaseapp.com",
+            databaseURL: "https://react-native-auth-a222b.firebaseio.com",
+            projectId: "react-native-auth-a222b",
+            storageBucket: "react-native-auth-a222b.appspot.com",
+            messagingSenderId: "270065831633"
+        });
+    }
+
     render() {
         return (
             <View>
